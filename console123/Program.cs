d7cefc7066c2ecf58b590e5bbaa97d96b635f7db
M0H1K4N <nejer911@gmail.com>
2025-05-03 21:38:28 +0400

    Add project files.

---
+++ console123/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Programm
+{
+    public static void Main(string[] args)
+    {
+
+
+
+
+
+        int myNumber = 0;
+        Console.WriteLine("myasdasdNumber = " + myNumber );
+
+    }
+}
