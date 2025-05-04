0f62720cc53502fcb3f5e76412ebe0b3c001a7b7
M0H1K4N <nejer911@gmail.com>
2025-05-04 14:11:19 +0400

    <asdasd2sdaasd>

--- console123/Program.cs
+++ console123/Program.cs
@@ -2,15 +2,131 @@
 
 public class Programm
 {
-    public static void Main(string[] args)
+    public static void Main(String[] args)
     {
 
-
-
-
         // this is new commit to the github
-        int myNumber = 0;
-        Console.WriteLine("myasdasdNumber = " + myNumber );
+        //string userName;
+        //userName = Console.ReadLine();
+
+        //int num1, num2;
+        //Console.WriteLine("First number please: ");
+        //num1 = Convert.ToInt32(Console.ReadLine());
+
+        //Console.WriteLine("Second number please: ");
+        //num2 = Convert.ToInt32(Console.ReadLine());
+
+        //Console.WriteLine($"Summ of those numbers are {num1 + num2}");
+
+        //Console.ReadLine();
+
+
+        // ARRAYS
+        //int[] myNumbers = new int[3];
+        //myNumbers[0] = 1;
+        //myNumbers[1]= 123;
+        //myNumbers[2] = 1123;
+        //Console.WriteLine(myNumbers[0] + myNumbers[2]);
+
+        //string[] userNames = { "Ali", "Veli", "Ayse", "Fatma", "Mehmet" };
+        //Console.WriteLine(userNames[2]);
+
+
+        // FUNCTIONS !!!!!!!!!
+
+        /// age calculator FUNCTION
+        // SayHello(12, 23);
+        //void SayHello(int birthYear, int currentYear)
+        //{
+        //    Console.WriteLine("When Where u born? type year:");
+        //    birthYear = Convert.ToInt32(Console.ReadLine());
+
+        //    Console.WriteLine("What is the current year?");
+        //    currentYear = Convert.ToInt32(Console.ReadLine());
+
+        //    Console.WriteLine( $"You are {currentYear - birthYear}, years old ");
+        //}
+
+
+        // IF / ELSE
+        //int num1 = 12;
+
+        //if (num1 > 10)
+        //{
+        //    Console.WriteLine("Number is greater than 10");
+        //}
+        //else if (num1 == 10)
+        //{
+        //    Console.WriteLine("Number is equal to 10");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Number is less than 10");
+
+        //}
+
+
+        // |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+        //CalculateFun(10, 20, "jami");
+
+        //void CalculateFun(int num1, int num2, string word)
+        //{
+        //    if (word == "jami")
+        //    {
+        //        Console.WriteLine(num1 + num2);
+        //    } else if(word == "skhvaoba")
+        //    {
+        //        Console.WriteLine(num2 - num1);
+        //    } else if(word == "namravli")
+        //    {
+        //        Console.WriteLine(num1 * num2);
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("error oqured");
+        //    }
+        //}
+        // |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+
+
+        // LOOPS IN C#
+        //int myNumber = 0;
+        //for (int i = 0; i < 10; i++)
+        //{
+        //    Console.WriteLine("Hello world!");
+
+        //    if(i == 4)
+        //    {
+        //        break;
+        //    }
+        //}
+        //////////////////////////////////////////////////
+        //string[] cars = { "BMW", "Mercedes", "Audi", "Tesla" };
+        /// 1 gza gamotanis
+        //foreach(string car in cars)
+        //{
+        //    Console.WriteLine(car);
+        //}
+
+        /// 2 gza gamotanis
+        //for(int i = 0; i < cars.Length; i++)
+        //{
+        //    Console.WriteLine(cars[i]);
+        //}
+        /////////////////////////////////////////
+        ///
+
+
+
+
+
+
+
+
+
+        Console.ReadLine(); 
+
 
     }
 }
